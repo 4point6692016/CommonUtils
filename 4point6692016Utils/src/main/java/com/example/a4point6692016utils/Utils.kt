42d3b10c738d78c768fe7c4b4b1ef6e70fcd475a
Prabanjan <prabanjan.gs@zohocorp.com>
2024-02-10 18:24:51 +0530

    Source Commit.

---
+++ 4point6692016Utils/src/main/java/com/example/a4point6692016utils/Utils.kt
@@ -0,0 +1,15 @@
+package com.example.a4point6692016utils
+
+import android.view.View
+
+fun View.makeVisible() {
+    visibility = View.VISIBLE
+}
+
+fun View.makeInvisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.makeGone() {
+    visibility = View.GONE
+}
