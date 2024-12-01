c886a078babc897fd687d31a40ca5521d790a16d
Prabanjan <prabanjan.gs@zohocorp.com>
2024-12-01 14:41:43 +0530

    More utils added.

--- 4point6692016Utils/src/main/java/com/example/a4point6692016utils/Utils.kt
+++ 4point6692016Utils/src/main/java/com/example/a4point6692016utils/Utils.kt
@@ -3,31 +3,113 @@
 package com.example.a4point6692016utils
 
 import android.app.Activity
+import android.app.ActivityManager
 import android.app.AlertDialog
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import android.os.Handler
 import android.os.Looper
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.ImageSpan
+import android.text.style.StyleSpan
+import android.util.DisplayMetrics
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.forEach
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.snackbar.Snackbar
 import me.zhanghai.android.fastscroll.FastScrollerBuilder
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
+
+// String Resources
+const val DATE_TIME_FORMAT = "MMM dd, yyyy, HH:mm:ss"
+const val DATE_FORMAT = "MMM dd, yyyy"
+const val TODAY = "Today"
+const val TOMORROW = "Tomorrow"
+const val YESTERDAY = "YESTERDAY"
+const val YEARS = "years"
+const val MONTHS = "months"
+const val HOURS = "hours"
+const val MILLISECONDS = "milliseconds"
+const val WEEKS = "weeks"
+const val MINUTES = "minutes"
+const val SECONDS = "seconds"
+const val DAYS = "days"
+const val YEARS_AGO = " $YEARS ago"
+const val DAYS_AGO = " $DAYS ago"
+const val HOURS_AGO = " $HOURS ago"
+const val MIN_AGO = " $MONTHS ago"
+const val MILLISECONDS_IN_A_YEAR = 31536000000 // 1000 * 60 * 60 * 24 * 365
+
+const val SOMETHING_WENT_WRONG = "Something went wrong!"
+
+const val OK = "OK"
+const val YES = "Yes"
+const val NO = "No"
+const val CANCEL = "Cancel"
+const val SAVE = "Save"
+const val JUST_NOW = "Just Now"
+const val NO_VALUE = "No Value"
+
+const val EMPTY = ""
+const val EMPTY_SPACE = " "
+const val COMMA = ","
+const val UNDERSCORE = "_"
+const val PERIOD = "."
+const val ZERO_STRING = "0"
+const val PERCENTAGE = "%"
+const val NULL = "null"
+const val NEW_LINE = "\n"
+
+const val TRUE = "true"
+const val FALSE = "false"
+
+const val CLOSE = "CLOSE"
+const val FINISH = "FINISH"
+
+const val FILE_TIMESTAMP_FORMAT = "yyyy.MM.dd.HH.mm.ss"
+
+const val RUPEE_SYMBOL = "₹"
+
+const val OR = "OR"
+const val AND = "AND"
+
+const val RECORD_PASSING_KEY = "recordPassingKey"
+
+const val RECORD_SAVED = "Record Saved Successfully!"
+
+// Others
+val locale: Locale = Locale.US
+val sdf = SimpleDateFormat(DATE_TIME_FORMAT, locale)
+val dateSDF = SimpleDateFormat(DATE_FORMAT, locale)
+fun mCalendar(): Calendar = Calendar.getInstance()
 
 fun View.makeVisible() {
     visibility = View.VISIBLE
@@ -57,6 +139,10 @@ fun Context.showAlert(message: String, positiveText: String, negativeText: Strin
         setNegativeButton(negativeText) { _, _ -> negativeAction.invoke() }
         this.create().show()
     }
+}
+
+fun String.ifNotEmpty(execute: (String) -> Unit){
+    if (isNotEmpty()) execute(this)
 }
 
 fun Int.getMonth(): String {
@@ -197,8 +283,26 @@ fun String.removeSpaces(): String {
     return replace("\\s".toRegex(), "")
 }
 
+fun String.replaceNewLineWithSpace(): String {
+    return replace(NEW_LINE, EMPTY_SPACE)
+}
+
 fun Context.getScreenWidthInPx(): Int {
     return resources.displayMetrics.widthPixels
+}
+
+fun Context.isDeviceNotificationDisabled(): Boolean {
+    return !isDeviceNotificationEnabled()
+}
+
+fun Context.isDeviceNotificationEnabled(): Boolean {
+    return NotificationManagerCompat.from(this).areNotificationsEnabled()
+}
+
+fun getScreenDimensions(windowManager: WindowManager): Pair<Int, Int> {
+    val displayMetrics = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(displayMetrics)
+    return Pair(displayMetrics.widthPixels, displayMetrics.heightPixels)
 }
 
 fun String.isStringNumeric(): Boolean {
@@ -209,16 +313,105 @@ fun String.isStringNumeric(): Boolean {
 fun RecyclerView.configureRVFastScroller(thumbRes: Int? = null) {
     FastScrollerBuilder(this).apply {
         thumbRes?.let { res ->
-            setThumbDrawable(ContextCompat.getDrawable(context, res)!!)
+            setThumbDrawable(context.getDrawableRes(res)!!)
         }
         build()
     }
 }
 
+fun View.isFocussedOrHasAChildInFocus(): Boolean {
+    return if (isFocused) {
+        true
+    } else {
+        if (this is ViewGroup) {
+            this.forEach {
+                if (it.isFocussedOrHasAChildInFocus()) {
+                    return true
+                }
+            }
+        }
+        false
+    }
+}
+
+fun View?.resolveAvailableWidth(): Int {
+    this ?: return 0
+    if (width >= 0) return width
+    if (layoutParams.width >= 0) return layoutParams.width
+    if (measuredWidth >= 0) return measuredWidth
+    return 0
+}
+
+fun View?.resolveAvailableHeight(): Int {
+    this ?: return 0
+    if (height >= 0) return height
+    if (layoutParams.height >= 0) return layoutParams.height
+    if (measuredHeight >= 0) return measuredHeight
+    return 0
+}
+
+fun Context.getDrawableRes(drawableResId: Int): Drawable? {
+    return ContextCompat.getDrawable(this, drawableResId)
+}
+
 fun Context.getColour(res: Int) = resources.getColor(res, null)
+
+fun Context.spanStringWithIcon(
+    actualText: SpannableString,
+    replaceText: String,
+    @DrawableRes iconRes: Int,
+    shouldIconThemeBased: Boolean = false,
+    iconWidth: Int = -1,
+    iconHeight: Int = -1
+): SpannableString {
+    return this.getDrawableRes(iconRes)?.also {
+        if (shouldIconThemeBased) {
+            it.setTint(0)
+        }
+        if (iconHeight != -1 && iconWidth != -1) {
+            it.setBounds(0, 0, iconWidth, iconHeight)
+        } else {
+            it.setBounds(0, 0, it.intrinsicHeight, it.intrinsicWidth)
+        }
+
+        val imageSpan = ImageSpan(it, ImageSpan.ALIGN_BOTTOM)
+        actualText.setSpan(
+            imageSpan,
+            actualText.indexOf(replaceText),
+            actualText.indexOf(replaceText) + replaceText.length,
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+    }?.let {
+        actualText
+    } ?: actualText
+}
 
 fun String.getStringWithBackgroundColor(colorCode: String = "#9CDAFF"): String {
     return "<span style='background-color:$colorCode'>$this</span>"
+}
+
+fun String.makeTextBold(startIndex: Int, lastIndex: Int, mode: Int = Spannable.SPAN_INCLUSIVE_INCLUSIVE): SpannableStringBuilder {
+    val spannableStringBuilder = SpannableStringBuilder(this)
+    val spanStyle = StyleSpan(Typeface.BOLD)
+    spannableStringBuilder.setSpan(spanStyle, startIndex, lastIndex, mode)
+    return spannableStringBuilder
+}
+
+fun String.getFullColouredString(color: String): SpannableString {
+    return getColouredString(0, length, color)
+}
+
+fun String.getColouredString(startIndex: Int, endIndex: Int, color: String): SpannableString {
+    val spannable = SpannableString(this)
+    spannable.setSpan(ForegroundColorSpan(Color.parseColor(color)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    return spannable
+}
+
+fun getSpannedStringForMandatoryField(value: String): SpannableString {
+    val spannableString = SpannableString("* $value ") // No I18N
+    spannableString.setSpan(ForegroundColorSpan(-0x12b7b8), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+    return spannableString
 }
 
 fun String.getTextToHighlight(searchQuery: String): String {
@@ -254,33 +447,6 @@ fun runOnUiThreadWithDelay(delay: Long = 200, runnable: Runnable) {
     Handler(Looper.getMainLooper()).postDelayed(runnable, delay)
 }
 
-object StringResources {
-    const val DATE_TIME_FORMAT = "MMM dd, yyyy, HH:mm:ss"
-    const val DATE_FORMAT = "MMM dd, yyyy"
-
-    const val SOMETHING_WENT_WRONG = "Something went wrong!"
-
-    const val OK = "OK"
-    const val YES = "Yes"
-    const val NO = "No"
-    const val CANCEL = "Cancel"
-    const val SAVE = "Save"
-
-    const val CLOSE = "CLOSE"
-    const val FINISH = "FINISH"
-
-    const val FILE_TIMESTAMP_FORMAT = "yyyy.MM.dd.HH.mm.ss"
-
-    const val RUPEE_SYMBOL = "₹"
-
-    const val OR = "OR"
-    const val AND = "AND"
-
-    const val RECORD_PASSING_KEY = "recordPassingKey"
-
-    const val RECORD_SAVED = "Record Saved Successfully!"
-}
-
 object DateTimeUtils {
 
     fun currentDateAndTime(now: Long = System.currentTimeMillis()): String {
@@ -300,7 +466,7 @@ object DateTimeUtils {
     fun thisYear() = mCalendar().get(Calendar.YEAR)
 
     fun currentTimeStampForFiles() : String {
-        return SimpleDateFormat(StringResources.FILE_TIMESTAMP_FORMAT, locale).format(System.currentTimeMillis())
+        return SimpleDateFormat(FILE_TIMESTAMP_FORMAT, locale).format(System.currentTimeMillis())
     }
 }
 
@@ -408,6 +574,18 @@ fun String.isInvalidAddSubExpression(): Boolean {
     return !regex.containsMatchIn(this)
 }
 
+fun Boolean.ifTrue(perform: () -> Unit) {
+    if (this) {
+        perform()
+    }
+}
+
+fun Boolean.ifFalse(perform: () -> Unit) {
+    if (!this) {
+        perform()
+    }
+}
+
 fun EditText.value() = text.toString()
 
 fun currentTimeInMSAsStr() = System.currentTimeMillis().toString()
@@ -423,7 +601,108 @@ fun String.isSingleValidOperand(): Boolean {
     }
 }
 
-val locale: Locale = Locale.US
-val sdf = SimpleDateFormat(StringResources.DATE_TIME_FORMAT, locale)
-val dateSDF = SimpleDateFormat(StringResources.DATE_FORMAT, locale)
-fun mCalendar(): Calendar = Calendar.getInstance()
+fun <T> ArrayList<T>.addIfNotAvailable(element: T) {
+    if (!contains(element)) {
+        add(element)
+    }
+}
+
+/**
+ * This is hack for viewpager2's children with different height to be expanded for their measured height
+ * not the previous child's measurement
+ * https://issuetracker.google.com/u/0/issues/143095219?pli=1
+ */
+fun ViewPager2.expandScreenItemHack(recyclerView: RecyclerView?): ViewPager2.OnPageChangeCallback {
+    fun measureAndUpdate(view: View) {
+        view.post {
+            val wMeasureSpec = View.MeasureSpec.makeMeasureSpec(view.width, View.MeasureSpec.EXACTLY)
+            val hMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            view.measure(wMeasureSpec, hMeasureSpec)
+            if (layoutParams.height != view.measuredHeight) {
+                layoutParams = (layoutParams as LinearLayout.LayoutParams).also { param ->
+                    param.height = view.measuredHeight
+                }
+            }
+        }
+    }
+    val listener = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            (recyclerView?.layoutManager as? LinearLayoutManager)?.findViewByPosition(position)?.let {
+                measureAndUpdate(it)
+            }
+        }
+    }
+    registerOnPageChangeCallback(listener)
+    return listener
+}
+
+fun SearchView.onQueryChange(action: (query: String) -> Boolean) {
+    setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return false
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            return action(query.toString())
+        }
+    })
+}
+
+fun <T> List<T>.contains(predicate: (T) -> Boolean): Boolean {
+    return find(predicate) != null
+}
+
+fun getLocationOnScreen(view: View): Pair<Int, Int> {
+    val outArray = IntArray(2)
+    view.getLocationOnScreen(outArray)
+    return Pair(outArray.first(), outArray.last())
+}
+
+fun Context.getMemoryInfo(): ActivityManager.MemoryInfo {
+    val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    return ActivityManager.MemoryInfo().also { memoryInfo ->
+        activityManager.getMemoryInfo(memoryInfo)
+    }
+}
+
+fun getRelativeTermFromMilliseconds(givenTime: Long): String {
+    val now = currentTimeInMS()
+    val diff = (now - givenTime).toDouble()
+    val year = MILLISECONDS_IN_A_YEAR
+    if ((diff / (1000 * 60)) < 1) {
+        return JUST_NOW
+    }
+    if ((diff / (1000 * 60)) >= 1 && (diff / (1000 * 60)) < 60) {
+        return "${(diff / (1000 * 60)).toInt()}" + MIN_AGO
+    }
+    if ((diff / (1000 * 60 * 60)) >= 1 && (diff / (1000 * 60 * 60)) < 24) {
+        return "${(diff / (1000 * 60 * 60)).toInt()}" + HOURS_AGO
+    }
+    if ((diff / (1000 * 60 * 60 * 24)) >= 1 && (diff / (1000 * 60 * 60 * 24)) < 365) {
+        return "${(diff / (1000 * 60 * 60 * 24)).toInt() + 1}" + DAYS_AGO
+    }
+    if ((diff / year) > 1) {
+        return "${(diff / year).toInt()}" + YEARS_AGO
+    }
+    return EMPTY
+}
+
+fun Long.getWeekOfDay(): String {
+    val calendar = mCalendar()
+    calendar.timeInMillis = this
+    return when (calendar.get(Calendar.DAY_OF_WEEK)) {
+        1 -> "SUNDAY"
+        2 -> "MONDAY"
+        3 -> "TUESDAY"
+        4 -> "WEDNESDAY"
+        5 -> "THURSDAY"
+        6 -> "FRIDAY"
+        7 -> "SATURDAY"
+        else -> EMPTY
+    }
+}
+
+fun Int.isLeapYear(): Boolean {
+    return (this % 4 == 0) && ((this % 100 != 0) || (this % 400 == 0))
+}
